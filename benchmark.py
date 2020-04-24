a115a50f5b4eba87bc26f0d8e8a7392f2f126d7f
zoeyf <50918942+zoeyf@users.noreply.github.com>
2020-04-23 23:24:10 -0400

    Update benchmark - generate graph based on nnode & nnedge

--- benchmark.py
+++ benchmark.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python
-# Measure performance
+# Measure overall performance
 # Adapted from https://github.com/cmu15418/asst3-s20/blob/master/code/benchmark.py
 
 import argparse
-import subprocess
-import sys
-import os
-import os.path
 import getopt
 import math
-import datetime
+import os
+import os.path
 import random
+import subprocess
+import sys
+import time
 
-# import rutil
+from regress import checkFiles
+from graph import generate_graph, graphName
 
 # General information
 stdProgram = "./johnson_boost"
 seqProgram = "./johnson_seq"
 ompProgram = "./johnson_omp"
 cudaProgram = "./johnson_cuda"
+
+graphProgram = "python3 graph.py"
 
 graphDirectory = "./graphs"
 
@@ -38,10 +41,11 @@ runCount = 3
 # How many mismatched lines warrant detailed report
 mismatchLimit = 5
 
-# Graph/rat combinations: testId : (graphFile, ratFile, test name)
+# Graph: (#node, #edge, #seed)
 benchmarkDict = {
-    'small':  'small.txt',
-    #'graph1': 'graph1.txt',
+    "A": (64,   200,    1),
+    "B": (512,  4000,   2),
+    "C": (1024, 10000,  3),
 }
 
 defaultTests = benchmarkDict.keys()
@@ -57,14 +61,8 @@ if host is not None and 'ghc' in host:
     threadLimit = 8
 defaultThreadCount = threadLimit
 threadCounts = [defaultThreadCount]
-#defaultSeed = rutil.DEFAULTSEED
 
 uniqueId = ""
-
-def trim(s):
-    while len(s) > 0 and s[-1] in '\r\n':
-        s = s[:-1]
-    return s
 
 def outmsg(s, noreturn = False):
     if len(s) > 0 and s[-1] != '\n' and not noreturn:
@@ -75,8 +73,7 @@ def outmsg(s, noreturn = False):
         outFile.write(s)
 
 def testName(testId, threadCount):
-    name = benchmarkDict[testId]
-    root = "%sx%.2d" % (name, threadCount)
+    root = "%sx%.2d" % (testId, threadCount)
     if uniqueId != "":
         root +=  ("-" + uniqueId)
     return root + ".txt"
@@ -84,145 +81,104 @@ def testName(testId, threadCount):
 def graphFileName(fname):
     return graphDirectory + '/' + fname
 
-def saveFileName(useRef, testId, stepCount, seed, threadCount):
-    return saveDirectory + "/" + ("ref" if useRef else "tst") + testName(testId, stepCount, seed, threadCount)
+def saveFileName(useRef, testId, threadCount):
+    return saveDirectory + "/" + ("ref-" if useRef else "tst-") + testName(testId, threadCount)
 
-def checkOutputs(referenceFile, testFile, tname):
-    if referenceFile == None or testFile == None:
-        return True
-    badLines = 0
-    lineNumber = 0
-    while True:
-        rline = referenceFile.readline()
-        tline = testFile.readline()
-        lineNumber +=1
-        if rline == "":
-            if tline == "":
-                break
-            else:
-                badLines += 1
-                outmsg("Test %s.  Mismatch at line %d.  Reference simulation ended prematurely" % (tname, lineNumber))
-                break
-        elif tline == "":
-            badLines += 1
-            outmsg("Test %s.  Mismatch at line %d.  Simulation ended prematurely\n" % (tname, lineNumber))
-            break
-        rline = trim(rline)
-        tline = trim(tline)
-        if rline != tline:
-            badLines += 1
-            if badLines <= mismatchLimit:
-                outmsg("Test %s.  Mismatch at line %d.  Expected result:'%s'.  Simulation result:'%s'\n" % (tname, lineNumber, rline, tline))
-    referenceFile.close()
-    testFile.close()
-    if badLines > 0:
-        outmsg("%d total mismatches.\n" % (badLines))
-    return badLines == 0
-
-def doRun(cmdList, simFileName):
+def doRun(cmdList, progFileName):
     cmdLine = " ".join(cmdList)
-    simFile = subprocess.PIPE
-    if simFileName is not None:
+    progFile = subprocess.PIPE
+    if progFileName is not None:
         try:
-            simFile = open(simFileName, 'w')
+            progFile = open(progFileName, 'w')
         except:
-            print("Couldn't open output file '%s'" % simFileName)
+            print("Couldn't open output file '%s'" % progFileName)
             return None
-    tstart = datetime.datetime.now()
+    tstart = time.perf_counter()
     try:
-        outmsg("Running '%s'" % cmdLine)
-        simProcess = subprocess.Popen(cmdList, stdout = simFile, stderr = subprocess.PIPE)
-        simProcess.wait()
-        if simFile != subprocess.PIPE:
-            simFile.close()
-        returnCode = simProcess.returncode
+        outmsg("Running '%s > %s'" % (cmdLine, progFileName))
+        progProcess = subprocess.Popen(cmdList, stdout = progFile, stderr = subprocess.PIPE)
+        progProcess.wait()
+        if progFile != subprocess.PIPE:
+            progFile.close()
+        returnCode = progProcess.returncode
         # Echo any results printed by simulator on stderr onto stdout
-        for line in simProcess.stderr:
+        for line in progProcess.stderr:
             outmsg(line)
     except Exception as e:
         print("Execution of command '%s' failed. %s" % (cmdLine, e))
-        if simFile != subprocess.PIPE:
-            simFile.close()
+        if progFile != subprocess.PIPE:
+            progFile.close()
         return None
     if returnCode == 0:
-        delta = datetime.datetime.now() - tstart
-        secs = delta.seconds + 24 * 3600 * delta.days + 1e-6 * delta.microseconds
-        if simFile != subprocess.PIPE:
-            simFile.close()
-        return secs
+        delta = time.perf_counter() - tstart
+        msecs = delta * 1e6
+        if progFile != subprocess.PIPE:
+            progFile.close()
+        return msecs
     else:
         print("Execution of command '%s' gave return code %d" % (cmdLine, returnCode))
-        if simFile != subprocess.PIPE:
-            simFile.close()
+        if progFile != subprocess.PIPE:
+            progFile.close()
         return None
 
-def bestRun(cmdList, simFileName):
+def bestRun(cmdList, progFileName):
     sofar = 1e6
     for r in range(runCount):
         if runCount > 1:
             outmsg("Run #%d:" % (r+1), noreturn = True)
-        secs = doRun(cmdList, simFileName)
+        secs = doRun(cmdList, progFileName)
         if secs is None:
             return None
         sofar = min(sofar, secs)
     return sofar
 
+def getGraph(nnode, nedge, seed):
+    gfname = graphName(nnode, nedge, seed)
+    if not os.path.exists(gfname):
+        # generate graph
+        generate_graph(nnode, nedge, seed)
+    return gfname
+
 def runBenchmark(useRef, testId, threadCount):
     global referenceFileName, testFileName
-    gfname = benchmarkDict[testId]
-    results = [testId, str(threadCount)]
+    nnode, nedge, seed = benchmarkDict[testId]
+    gfname = getGraph(nnode, nedge, seed)
+    results = [nnode, nedge, seed, str(threadCount)]
     prog = stdProgram if useRef else seqProgram if threadCount == 1 else ompProgram
     clist = ["-g", graphFileName(gfname)]#, "-t", str(threadCount)]
     # if doInstrument:
     #     clist += ["-I"]
-    simFileName = None
+    fileName = None
     if not useRef:
         name = testName(testId, threadCount)
         outmsg("+++++++++++++++++ Benchmark %s +++++++++++++++++" % name)
-    # if doCheck:
-    #     if not os.path.exists(saveDirectory):
-    #         try:
-    #             os.mkdir(saveDirectory)
-    #         except Exception as e:
-    #             outmsg("Couldn't create directory '%s' (%s)" % (saveDirectory, str(e)))
-    #             simFile = subprocess.PIPE
-    #     clist += ["-i", str(stepCount)]
-    #     simFileName = saveFileName(useRef, testId, threadCount)
-    #     if useRef:
-    #         referenceFileName = simFileName
-    #     else:
-    #         testFileName = simFileName
-    # else:
-    # clist += ["-q"]
+    if doCheck:
+        if not os.path.exists(saveDirectory):
+            try:
+                os.mkdir(saveDirectory)
+            except Exception as e:
+                outmsg("Couldn't create directory '%s' (%s)" % (saveDirectory, str(e)))
+                progFile = subprocess.PIPE
+        fileName = saveFileName(useRef, testId, threadCount)
+        if useRef:
+            referenceFileName = fileName
+        else:
+            testFileName = fileName
 
     cmd = [prog] + clist
     cmdLine = " ".join(cmd)
-    secs = bestRun(cmd, simFileName)
+    secs = bestRun(cmd, fileName)
     if secs is None:
         return None
     else:
-        # rmoves = (nodes * load) * stepCount
-        # npm = 1e9 * secs/rmoves
         results.append("%.2f" % secs)
-        # results.append("%.2f" % npm)
         return results
 
-def score(npm, rnpm):
-    if npm == 0.0:
-        return 0
-    ratio = rnpm/npm
-    nscore = 0.0
-    if ratio >= upperThreshold:
-        nscore = 1.0
-    elif ratio >= lowerThreshold:
-        nscore = (ratio-lowerThreshold)/(upperThreshold - lowerThreshold)
-    return int(math.ceil(nscore * pointsPerRun))
-
 def formatTitle():
-    ls = ["Name", "threads", "secs"]
-    # if doCheck:
-    #     ls += ["BNPM", "Ratio", "Pts"]
-    return "\t".join(ls)
+    ls = ["# Node", "# Edge", "Seed", "Threads", "Test (ms)"]
+    if doCheck:
+         ls += ["Ref (ms)"]
+    return " ".join("{0:<10}".format(t) for t in ls)
 
 def sweep(testList, threadCount):
     tcount = 0
@@ -234,55 +190,24 @@ def sweep(testList, threadCount):
     totalPoints = 0
     for t in testList:
         ok = True
-
         results = runBenchmark(False, t, threadCount)
-        # if results is not None and doCheck:
-        #     cresults = runBenchmark(True, t, stepCount, threadCount)
-        #     if referenceFileName != "" and testFileName != "":
-        #         try:
-        #             rfile = open(referenceFileName, 'r')
-        #         except:
-        #             rfile = None
-        #             print "Couldn't open reference simulation output file '%s'" % referenceFileName
-        #             ok = False
-        #         try:
-        #             tfile = open(testFileName, 'r')
-        #         except:
-        #             tfile = None
-        #             print "Couldn't open test simulation output file '%s'" % testFileName
-        #             ok = False
-        #         if rfile is not None and tfile is not None:
-        #             ok = checkOutputs(rfile, tfile, t)
-        # if not ok:
-        #     outmsg("TEST FAILED.  0 points")
+        if results is not None and doCheck:
+            cresults = runBenchmark(True, t, threadCount)
+            if referenceFileName != "" and testFileName != "":
+                ok = checkFiles(referenceFileName, testFileName)
+        if not ok:
+            outmsg("TEST FAILED")
         if  results is not None:
             tcount += 1
-            npm = float(results[-1])
-            sum += npm
-            # if cresults is not None:
-            #     rcount += 1
-            #     cnpm = float(cresults[-1])
-            #     refSum += cnpm
-            #     ratio = cnpm/npm if npm > 0 else 0
-            #     points = score(npm, cnpm) if ok else 0
-            #     totalPoints += points
-            #     results += [cresults[-1], "%.3f" % ratio, "%d" % points]
+            if cresults is not None:
+                msecs = cresults[-1]
+                results += [msecs]
             resultList.append(results)
-    outmsg("+++++++++++++++++")
+    outmsg("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     outmsg(formatTitle())
-    outmsg("+++++++++++++++++")
-    for r in resultList:
-        outmsg("\t".join(r))
-    if tcount > 0:
-        avg = sum/tcount
-        astring = "AVG:\t\t\t\t%.2f" % avg
-        if refSum > 0:
-            ravg = refSum/rcount
-            astring += "\t%.2f" % ravg
-        outmsg(astring)
-        # if doCheck:
-        #     tstring = "TOTAL:\t\t\t\t\t\t\t%d" % totalPoints
-        #     outmsg(tstring)
+    outmsg("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    for result in resultList:
+        outmsg(" ".join("{0:<10}".format(r) for r in result))
 
 def generateFileName(template):
     global uniqueId
@@ -308,16 +233,14 @@ def run():
     if gpu:
         raise NotImplementedException();
     else:
-        gstart = datetime.datetime.now()
+        gstart = time.perf_counter()
         for t in threadCounts:
-            tstart = datetime.datetime.now()
+            tstart = time.perf_counter()
             sweep(testList, t)
-            delta = datetime.datetime.now() - tstart
-            secs = delta.seconds + 24 * 3600 * delta.days + 1e-6 * delta.microseconds
+            secs = time.perf_counter() - tstart
             print("Test time for %d threads = %.2f secs." % (t, secs))
         if len(threadCounts) > 1:
-            delta = datetime.datetime.now() - gstart
-            secs = delta.seconds + 24 * 3600 * delta.days + 1e-6 * delta.microseconds
+            secs = time.perf_counter() - gstart
             print("Overall test time = %.2f secs." % (secs))
 
 if __name__ == "__main__":
